33ebd2e3ea48f13003fe63eeee5395fb0dfe4e41
annerh3 <annerh3@gmail.com>
2024-04-04 22:56:16 -0600

    okey. pude terminar el primero

--- js/divisas.js
+++ js/divisas.js
@@ -7,15 +7,21 @@
 // document.addEventListener('DOMContentLoaded', () => {
 //     console.log(2);
 // })
-// alert('Utilice los Botones para Seleccionar la cantidad')
+alert('Utilice los Botones para Seleccionar la cantidad')
+
+// estas variables las dejaré declaradas nada mas (las utilizaré dentro de los eventos y dentro de la funcion convertirDivisa)
+let valorDeOpcionSeleccionadaFROM;
+let valorDeOpcionSeleccionadaTO;
+let cantidad = 0;
 
 const cantidadBox = document.querySelector('#amount');
 console.log(cantidadBox);
-    cantidadBox.addEventListener('click', () => {
-        console.log('Click en cantidadBox');
-        const cantidad = document.querySelector('#amount').valueAsNumber;
-        console.log(cantidad );
-    });
+
+cantidadBox.addEventListener('click', () => {
+    console.log('Click en cantidadBox');
+    cantidad = document.querySelector('#amount').valueAsNumber;
+    console.log(cantidad );
+});
 
 // const seleccionarDivisaAconvertir = document.querySelector('#from');
 // console.log(seleccionarDivisaAconvertir);
@@ -24,12 +30,72 @@ console.log(cantidadBox);
 
 // object.addEventListener("change", myScript);
 
-const seleccionarDivisaAconvertir = document.getSelection('#from');
-console.log(seleccionarDivisaAconvertir);   
-seleccionarDivisaAconvertir.addEventListener(".onChange", () => {
-    console.log('desde change');
+// const seleccionarDivisaAconvertir = document.getSelection('#from');
+// console.log(seleccionarDivisaAconvertir);   
+// seleccionarDivisaAconvertir.addEventListener(".onChange", () => {
+//     console.log('desde change');
+// });
+
+// EL CAPTURAR LOS VALORES DE LOS SELECTORES FUE MI BLOQUEO, INGE. NO LOGRABA LA CAPURA DE ESOS ELEMENTOS
+// HASTA QUE ENCONTRÉ EN LA DOCUMETACION COMO HACERLO (ya con mas calma) 
+
+ //capturar value del primer input
+const seleccionarDivisaAconvertir = document.getElementById("from");
+console.log(seleccionarDivisaAconvertir);
+
+seleccionarDivisaAconvertir.addEventListener('change', () => {
+    const opcionSeleccionada = seleccionarDivisaAconvertir.options[seleccionarDivisaAconvertir.selectedIndex];
+    console.log(opcionSeleccionada);
+    valorDeOpcionSeleccionadaFROM = opcionSeleccionada.value;
+    console.log(valorDeOpcionSeleccionadaFROM);
 });
 
-// seleccionarDivisaAconvertir.addEventListener('click', () => {
-//     console.log('desde evento seleccionarDivisa');
-// });
+//capturar value del segundo input
+const seleccionarDivisaAlaQueSeConvertira = document.getElementById("to");
+console.log(seleccionarDivisaAlaQueSeConvertira);
+
+seleccionarDivisaAlaQueSeConvertira.addEventListener('change', () => {
+    const opcionSeleccionadaTO = seleccionarDivisaAlaQueSeConvertira.options[seleccionarDivisaAlaQueSeConvertira.selectedIndex];
+    console.log(opcionSeleccionadaTO);
+    valorDeOpcionSeleccionadaTO = opcionSeleccionadaTO.value;
+    console.log(valorDeOpcionSeleccionadaTO);
+    
+});
+
+// calcular la conversion
+
+const tasasDeConversion = {
+                                usd: {
+                                    eur: 0.88,
+                                    lps: 24.7296},
+                                eur: {
+                                    usd: 1.14,
+                                    lps: 26.88},
+                                lps: {
+                                    usd: 0.040,
+                                    eur: 0.037}
+                        };
+
+function convertirDivisa() {
+    console.log(valorDeOpcionSeleccionadaFROM);
+    console.log(valorDeOpcionSeleccionadaTO);
+    console.log(cantidad);
+
+    if (valorDeOpcionSeleccionadaFROM === valorDeOpcionSeleccionadaTO) {
+        alert('Esta Conversión no es válida. Cambie las divisas.');
+    } else {
+        console.log('Pasó la validación inicial');
+        const factorConversion = tasasDeConversion[valorDeOpcionSeleccionadaFROM][valorDeOpcionSeleccionadaTO]; //  acceder a la tasa de conversion entre dos las divisas seleccionadas
+        const resultado = cantidad * factorConversion;
+        mostrarEnHTML(resultado);
+        console.log(resultado);
+}
+}
+// funcion para mostrar el resultado en el html
+function mostrarEnHTML(respuesta) {
+    const div = document.getElementById('result-container');
+    div.innerHTML = '';
+    const respuestaAmostrar = document.createElement('div');
+    respuestaAmostrar.textContent = respuesta;
+    div.appendChild(respuestaAmostrar);
+}
--- js/pumagram.js
+++ js/pumagram.js
@@ -1,10 +1,27 @@
-const likes = document.querySelector('.like-btn');
+const btnlikes = document.querySelector('.like-btn');
 let contadorLikes = 0;
-const likeCount = document.querySelector('.actions');
-console.log(likeCount);
+const likeCount = document.querySelector(".like-count");
+// const likeCount = document.querySelector('.actions');
+// console.log(likeCount);
 
-likes.addEventListener( 'click', () => {
-    console.log('Like');
-    contadorLikes++;
-    likeCount.appendChild(contadorLikes);
-})
+if(contadorLikes==0){
+    btnlikes.addEventListener("click", function() {
+        if(contadorLikes==0){
+        contadorLikes++;
+        likeCount.textContent = contadorLikes;
+        btnlikes.textContent = "No me gusta";
+        localStorage.setItem("likes", contadorLikes);
+        }
+      });
+}else{
+    btnlikes.addEventListener("click", function() {
+        if(contadorLikes==1){
+        contadorLikes--;
+        likeCount.textContent = contadorLikes;
+        btnlikes.textContent = "Me gusta";
+        localStorage.setItem("likes", contadorLikes);
+        }
+      });
+}
+
+// solo logré hacer ésto  :(
